d62bdbf6285edcf1184d50bc166cd08d2cbf3dca
YuTony <versities@gmail.com>
2021-10-22 06:35:35 +0300

    Работа над интерфейсом

--- client_app.py
+++ client_app.py
@@ -1,44 +1,9 @@
 import logging
-import socket
-import ssl
 import sys
 
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 
 from client import Status, Client
-
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-ssl_context.load_verify_locations("./selfsigned.crt")
-
-
-# async def hello():
-#     uri = "wss://localhost:8080"
-#     async with websockets.connect(uri, ssl=ssl_context) as websocket:
-#         name = input("What's your name? ")
-#
-#         await websocket.send(name)
-#         print(f">>> {name}")
-#
-#         greeting = await websocket.recv()
-#         print(f"<<< {greeting}")
-
-# asyncio.run(hello())
-
-# def mysend(msg, sock):
-#     totalsent = 0
-#     while totalsent < MSGLEN:
-#         sent = sock.send(msg[totalsent:])
-#         if sent == 0:
-#             raise RuntimeError("socket connection broken")
-#         totalsent = totalsent + sent
-
-def main():
-    hostname = 'localhost'
-    with socket.create_connection((hostname, 8080)) as sock:
-        with ssl_context.wrap_socket(sock, server_hostname=hostname) as ssock:
-            print(ssock.version())
-            print(ssock.send(b'hello'))
-            # ssock.shutdown(socket.SHUT_RDWR)
 
 
 class ClientApp(QtWidgets.QWidget):
@@ -59,11 +24,30 @@ class ClientApp(QtWidgets.QWidget):
         self.text_field = QtWidgets.QTextEdit()
         self.label = QtWidgets.QLabel()
 
+        self.label.setStyleSheet("border: 1px solid black")
+        # self.label
+
+        self.ip_layout = QtWidgets.QHBoxLayout()
+        self.ip_layout.addWidget(QtWidgets.QLabel("Адрес:"))
+        self.ip_field = QtWidgets.QLineEdit()
+        self.ip_layout.addWidget(self.ip_field)
+
+        self.port_layout = QtWidgets.QHBoxLayout()
+        self.port_layout.addWidget(QtWidgets.QLabel("Port:"))
+        self.port_field = QtWidgets.QLineEdit()
+        self.port_layout.addWidget(self.port_field)
+
         self.layout = QtWidgets.QVBoxLayout(self)
 
+        self.layout.addLayout(self.ip_layout)
+        self.layout.addLayout(self.port_layout)
         self.layout.addWidget(self.button_connect)
         self.layout.addWidget(self.button_disconnect)
+        self.layout.addSpacing(10)
+        self.layout.addWidget(QtWidgets.QLabel("Получено сообщение:"))
         self.layout.addWidget(self.label)
+        self.layout.addSpacing(10)
+        self.layout.addWidget(QtWidgets.QLabel("Новое сообщение:"))
         self.layout.addWidget(self.text_field)
         self.layout.addWidget(self.button_send_msg)
 
@@ -74,16 +58,15 @@ class ClientApp(QtWidgets.QWidget):
         self.set_buttons_status(Status.DISCONNECTED)
 
     def connect_to_server(self):
-        self.client.connect(self.CERT_FILE)
+        self.client.connect(self.CERT_FILE, self.label.setText)
 
     def close_connection(self):
-        self.disconnect()
+        self.client.disconnect()
 
     def send_msg(self):
         self.client.send_msg(self.text_field.toPlainText())
 
     def set_buttons_status(self, status: Status):
-        # TODO: set_buttons_status
         if status == Status.CONNECTED:
             self.button_connect.setDisabled(True)
             self.button_disconnect.setDisabled(False)
@@ -100,18 +83,21 @@ class ClientApp(QtWidgets.QWidget):
             self.button_send_msg.setDisabled(True)
             self.text_field.setDisabled(False)
 
+    def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
+        self.client.disconnect()
+        a0.accept()
+
 
 def start_client_app():
     app = QtWidgets.QApplication([])
 
     widget = ClientApp()
     widget.setWindowTitle("Client")
-    widget.resize(800, 600)
-    widget.move(830, 50)
+    widget.resize(400, 300)
+    widget.move(430, 50)
     widget.show()
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    # main()
     start_client_app()
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import ssl
 import threading
 from enum import Enum
 from typing import Callable
+import os
 
 
 class Status(Enum):
@@ -13,7 +14,7 @@ class Status(Enum):
 
 
 class Server:
-    def __init__(self, change_status: Callable[[Status], None]):
+    def __init__(self, change_status: Callable[[Status], None], msg_handler: Callable[[str], None]):
         self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
         self.change_status = change_status
@@ -25,53 +26,57 @@ class Server:
         self.conn: ssl.SSLSocket | None = None
         self.addr = None
 
+        self.wait_thread: threading.Thread | None = None
         self.msg_loop_thread: threading.Thread | None = None
         self.is_msg_loop = False
 
-    def start(self, host: str, port: int, crt: str, privat_key: str, msg_handler: Callable[[str], None]):
+        self.msg_handler: Callable[[str], None] = msg_handler
+
+    def start(self, host: str, port: int, crt: str, privat_key: str):
         self.context.load_cert_chain(crt, privat_key)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         self.sock.bind((host, port))
         self.sock.listen(1)
         self.ssock = self.context.wrap_socket(self.sock, server_side=True)
-        # self.ssock.setblocking(False)
-        self.msg_loop_thread = threading.Thread(target=self.wait_connect, args=(msg_handler, ))
-        self.msg_loop_thread.start()
+        self.wait_connect()
 
-    def wait(self, set_text: Callable[[str], None]):
-        self.context.load_cert_chain('./selfsigned.crt', './private.key')
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
-            sock.bind(('localhost', 8080))
-            sock.listen(5)
-            with self.context.wrap_socket(sock, server_side=True) as ssock:
-                conn, addr = ssock.accept()
-                # print(conn.read(64).decode('utf-8'))
-                set_text(conn.read(64).decode('utf-8'))
+    def wait_connect(self):
+        self.wait_thread = threading.Thread(target=self._wait_connect)
+        self.wait_thread.start()
 
-    def wait_connect(self, msg_handler: Callable[[str], None]):
+    def _wait_connect(self):
         try:
             self.change_status(Status.WAITING)
             self.conn, self.addr = self.ssock.accept()
             self.change_status(Status.CONNECTED)
             self.logger.info(f'Connected {self.addr}')
-            self.msg_loop(msg_handler)
+            self.msg_loop()
         except OSError:
             self.logger.info('Socket.accept is forced to terminate')
 
     def send_msg(self, msg: str):
         if self.conn:
-            self.conn.send(bytes(msg, "utf-8"))
+            try:
+                a = self.conn.send(bytes(msg, "utf-8"))
+                print(a)
+            except OSError as err:
+                self.conn.close()
+                self.wait_connect()
+                print(err)
 
-    def msg_loop(self, msg_handler: Callable[[str], None]):
+    def msg_loop(self):
+        self.msg_loop_thread = threading.Thread(target=self._msg_loop)
+        self.msg_loop_thread.start()
+
+    def _msg_loop(self):
         self.is_msg_loop = True
         self.logger.info('msg loop start')
         while self.is_msg_loop:
             data = self.conn.recv(1024)
-            print(data)
             if data != b'':
                 msg = data.decode("utf-8")
                 self.logger.info(f'Message received from {self.addr}: {msg}')
-                msg_handler(msg)
+                self.msg_handler(msg)
         self.logger.info('msg loop stop')
 
     def stop(self):
@@ -82,15 +87,4 @@ class Server:
                 self.conn = None
             self.ssock.close()
             self.ssock = None
-            # if self.is_msg_loop:
-            #     self.is_msg_loop = False
-            #     # self.msg_loop_thread.join()
-            #     self.ssock.close()
-            # else:
-            #     self.ssock.close()
-                # self.msg_loop_thread.join()
-            # self.is_msg_loop = False
-            # self.ssock.close()
-            # if self.msg_loop_thread:
-            #     self.msg_loop_thread.join()
         self.change_status(Status.STOPPED)
